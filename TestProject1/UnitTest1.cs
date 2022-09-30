39e112be3b7ce405b3737c6b0954608ca704e042
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-30 20:08:39 +0530

    [Lokesh]Added:As a User need to enter a valid phoneNumber

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -1,11 +1,67 @@
+using UserRegistrationExceptionHandling;
+
 namespace TestProject1
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly Patterns patternsMatch = new Patterns();
         [TestMethod]
-        public void TestMethod1()
+        
+            public void GivenFirstName_ShouldThrow_UserRegistrationCustomException()
+            {
+                try
+                {
+                    bool expected = true;
+                    Assert.AreEqual(expected, patternsMatch.ValidateFirstName(null));
+                }
+                catch (UserRegistrationCustomException e)
+                {
+                    Assert.AreEqual("Invalid FirstName", e.Message);
+
+                }
+            }
+            [TestMethod]
+            public void GivenLastName_ShouldThrow_UserRegistrationCustomException()
+            {
+                try
+                {
+                    bool expected = true;
+                    Assert.AreEqual(expected, patternsMatch.ValidateLastName(null));
+                }
+                catch (UserRegistrationCustomException e)
+                {
+                    Assert.AreEqual("Invalid LastName", e.Message);
+                }
+            }
+
+        [TestMethod]
+        public void GivenEmailId_ShouldThrow_UserRegistrationCustomException()
         {
+            try
+            {
+                bool expected = true;
+                Assert.AreEqual(expected, patternsMatch.ValidateEmailId(null));
+            }
+            catch (UserRegistrationCustomException e)
+            {
+                Assert.AreEqual("Invalid Email ID", e.Message);
+            }
         }
+
+        [TestMethod]
+        public void GivenMobileNumber_ShouldThrow_UserRegistrationCustomException()
+        {
+            try
+            {
+                bool expected = true;
+                Assert.AreEqual(expected, patternsMatch.ValidateMobileNumber(null));
+            }
+            catch (UserRegistrationCustomException e)
+            {
+                Assert.AreEqual("Invalid Mobile Number", e.Message);
+            }
+        }
+
     }
 }
